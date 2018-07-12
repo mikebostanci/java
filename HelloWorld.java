d6a7f6d80871fa7b29868290225dc6b17ee4c669
Mike Bostanci <mikebostanci@yahoo.com>
2018-07-11 22:53:29 -0400

    Two parameters to run it from jenkins

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,8 @@
 //third from local to github --mike
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, world");
-		System.out.println(args[0]);
-		System.out.println(args[0]);
+		System.out.println("Hello, world with parameter running at Jenkins ");
+		System.out.println(args[0] || " This is the second parameter: " ||  args[1] );
+//		System.out.println(args[0);
        }
 }
